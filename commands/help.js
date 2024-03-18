a274fce29c5e9d55060d3dff6db12fc3ca239df7
Kaizaki26 <159022841+Kaizaki26@users.noreply.github.com>
2024-03-18 18:09:55 +0800

    help.js

--- commands/help.js
+++ commands/help.js
@@ -57,24 +57,7 @@ module.exports = {
            { name: '🎵 Now Playing', value: 'Display the currently playing song information' },
           { name: '🔊 Volume', value: 'Adjust the music volume [ hearing at high volumes is risky ]' },
         ) 
-       .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
-      const button1 = new ButtonBuilder()
-        .setLabel('YouTube')
-        .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
-        .setStyle(ButtonStyle.Link);
-
-      const button2 = new ButtonBuilder()
-        .setLabel('Discord')
-        .setURL('https://discord.gg/FUEHs7RCqz')
-        .setStyle(ButtonStyle.Link);
-
-      const button3 = new ButtonBuilder()
-        .setLabel('Code')
-        .setURL('https://github.com/RTX-GAMINGG/RTX-MUSIC-BOT-v3.6')
-        .setStyle(ButtonStyle.Link);
-
-      const row = new ActionRowBuilder()
-        .addComponents(button1, button2, button3);
+       .setImage('https://cdn.discordapp.com/attachments/1191724147454251069/1219226148756652123/image.png?ex=660a87d5&is=65f812d5&hm=bfedaddb508dca1d436d7ae1452c56487fc63590030de765c396af832acb6c49&')
 
       interaction.reply({
         embeds: [musicCommandsEmbed, basicCommandsEmbed],
